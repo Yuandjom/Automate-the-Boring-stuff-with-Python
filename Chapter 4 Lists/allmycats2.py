4fd6362edffa99499616af1969b67e18e3cd2f92
Yuandjom <ziyangjohn@gmail.com>
2020-10-20 15:42:21 +0800

    wassup

---
+++ Chapter 4 Lists/allmycats2.py
@@ -0,0 +1,10 @@
+catNames = []
+while True :
+    print("Enter the name of the cat" + str(len(catNames) + 1 ) + ' (or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames.append(name) #or catNames + [name] list concentration 
+print("The cat names are:")
+for name in catNames:
+    print(name)
