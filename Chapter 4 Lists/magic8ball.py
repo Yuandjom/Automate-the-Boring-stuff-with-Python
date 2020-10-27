af9bb56319a5c54074f1532bd96359b4da72f194
Yuandjom <ziyangjohn@gmail.com>
2020-10-27 22:25:52 +0800

    Hello

---
+++ Chapter 4 Lists/magic8ball.py
@@ -0,0 +1,12 @@
+import random
+
+messages = [
+    'It is certain',
+    'it is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Outlook not so good',
+    'Very doubtful'
+]
+
+print(messages[random.randint(0,len(messages)-1)])
