fe55fbcc1b9106d6c4c5707e2b86396cec703243
Yuandjom <ziyangjohn@gmail.com>
2020-11-01 19:11:15 +0800

    hi

---
+++ Chapter 5 Dictionaries/totalbrought.py
@@ -0,0 +1,8 @@
+allGuests = {'Alice':{'apples':5,'pretzels':12},
+            'Bob':{'ham sandwiches':3,'apples':2},
+            'Carol':{'cups':3,'apple pies':1}
+
+
+}
+def totalBrought(guests,item):
+    numBrought= 
