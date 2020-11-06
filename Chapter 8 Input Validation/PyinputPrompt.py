844b3de6a30e77ee96229b92fe9972cd2571677d
Yuandjom <ziyangjohn@gmail.com>
2020-11-06 22:02:56 +0800

    done

---
+++ Chapter 8 Input Validation/PyinputPrompt.py
@@ -0,0 +1,8 @@
+import pyinputplus as pyip 
+
+response = pyip.inputInt(prompt = "Enter a number: ")
+print(response)
+
+#can use python help() function to find out more about the function 
+# eg. help(pyip.inputInt) 
+
---
+++ Chapter 8 Input Validation/listenumerate.py
@@ -0,0 +1,2 @@
+number = list('123456')
+print(number)
